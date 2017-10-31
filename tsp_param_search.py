49dbbd7e7324e6715eaaa44390f920c7130bec6c
Carlo Rapisarda <rapisarda.carlo@gmail.com>
2017-10-31 20:57:02 +0100

    Fix minor bug, add CPU time in results, add option to export results

--- tsp_param_search.py
+++ tsp_param_search.py
@@ -9,9 +9,11 @@
 ##
 
 import os
+import csv
 import subprocess
 import argparse
 import glob
+import time
 import multiprocessing as mp
 from numpy import linspace
 
@@ -20,15 +22,43 @@ DEF_EXE_PATH = "./TSP"
 DEF_INPUT_FOLDER = "./in/"
 
 parser = argparse.ArgumentParser(prog='TSP Parameter Optimization', usage='Some usage', description='Some description')
-parser.add_argument('-e', '--exepath',   help='path to the executable', default=DEF_EXE_PATH, type=str)
-parser.add_argument('-i', '--inputfolder',   help='path to the folder with the input files', default=DEF_INPUT_FOLDER, type=str)
+parser.add_argument('-e', '--exepath',   	help='path to the executable', default=DEF_EXE_PATH, type=str)
+parser.add_argument('-i', '--inputfolder',  help='path to the folder with the input files', default=DEF_INPUT_FOLDER, type=str)
+parser.add_argument('-o', '--outputfile',   help='path to the output csv file with results', default=None, type=str)
 args = parser.parse_args()
 
 exe_path = args.exepath
+output_filepath = args.outputfile
 input_folder = args.inputfolder
 input_filepaths = glob.glob(input_folder+"/*")
 
 print("Found {} input files".format(len(input_filepaths)))
+
+
+
+def results_to_csv(results, csv_path):
+
+	if not csv_path.endswith(".csv"):
+		csv_path += ".csv"
+
+	print("\nWriting results to CSV file...")
+
+	with open(csv_path, 'w+', encoding='utf-8') as csvfile:
+		writer = csv.writer(
+			csvfile,
+			delimiter=',',
+			quotechar='"',
+			quoting=csv.QUOTE_MINIMAL
+		)
+		for r in results:
+			writer.writerow([
+				r['noise_ratio'],
+				r['noise_period'],
+				r['threeopt_threshold'],
+				r['total_length'],
+				r['total_time']
+			])
+	print("Wrote to {}".format(csv_path))
 
 
 
@@ -39,7 +69,8 @@ def bf_init(results_arg):
 def bf_run(params):
 
 	input_filepaths = params["input_filepaths"]
-	total = 0
+	total_length = 0
+	total_time = 0.0
 
 	for fp in input_filepaths:
 
@@ -49,16 +80,22 @@ def bf_run(params):
 			noise_ratio = params["noise_ratio"],
 			noise_period = params["noise_period"],
 			threeopt_threshold = params["threeopt_threshold"]
-			)
+		)
+		tick = time.perf_counter()
 		res = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
-		res = int(res.stdout.decode('utf-8'))
-		total += res
+		tock = time.perf_counter()
+		length = int(res.stdout.decode('utf-8'))
+		total_length += length
+		total_time += (tock-tick)
 
 	del params["exe_path"]
 	del params["input_filepaths"]
-	params["total_length"] = res
+	params["total_length"] = total_length
+	params["total_time"] = total_time
 	results.append(params)
 	print("Job {} -> {}".format(len(results), params))
+
+
 
 def parallel_bf():
 
@@ -71,12 +108,12 @@ def parallel_bf():
 		for np in noise_period_ls:
 			for tt in threeopt_threshold_ls:
 				jobs.append({
-							"exe_path": exe_path,
-							"noise_ratio": nr,
-							"noise_period": np,
-							"threeopt_threshold": tt,
-							"input_filepaths": input_filepaths
-						})
+					"exe_path": exe_path,
+					"noise_ratio": nr,
+					"noise_period": np,
+					"threeopt_threshold": tt,
+					"input_filepaths": input_filepaths
+				})
 
 	jobs_count = len(jobs)
 	print("Total jobs: {}".format(jobs_count))
@@ -89,57 +126,27 @@ def parallel_bf():
 	pool.map(bf_run, jobs)
 
 	# Sort by total_length
-	results = sorted(results, key = lambda res : res["total_length"])
+	results_by_length = sorted(results, key = lambda res : res["total_length"])
 
-	best_res = results[0]
-	best_len = best_res["total_length"]
-	shared_count = len(list(filter(lambda res : res["total_length"] == best_len, results)))-1
+	# Sort by total_time
+	results_by_time = sorted(results, key = lambda res : res["total_time"])
 
-	print()
-	print("Best result: {}".format(best_res))
+	best_by_length = results_by_length[0]
+	best_by_time = results_by_time[0]
+
+	best_len = best_by_length["total_length"]
+	best_time = best_by_time["total_time"]
+
+	shared_count = len(list(filter(lambda res : res["total_length"] == best_len, results_by_length)))-1
+
+	print("\nBest by length: {}".format(best_by_length))
 	print("Score shared with {} other combinations of parameters ({}%)".format(shared_count, round(100.0*shared_count/(jobs_count-1), 2)))
+	print("\nBest by time: {}".format(best_by_time))
 
-
-## Old code, use parallelized BF
-def brute_force():
-
-	noise_ratio_ls = linspace(0.5, 3.0, num=20)
-	noise_period_ls = linspace(1.0, 10.0, num=10)
-	threeopt_threshold_ls = linspace(30.0, 30.0, num=1)
-
-	total_iters = len(noise_ratio_ls) * len(noise_period_ls) * len(threeopt_threshold_ls)
-	print("Total iterations: {}".format(total_iters))
-
-	best_total = 0
-	best_nr = 0
-	best_np = 0
-	best_tt = 0
-
-	for nr in noise_ratio_ls:
-		for np in noise_period_ls:
-			for tt in threeopt_threshold_ls:
-
-				total = 0
-				
-				for fp in input_filepaths:
-
-					res = os.system("{exe_path} {noise_ratio} {noise_period} {threeopt_threshold} < {input_filepath}".format(
-							exe_path = exe_path,
-							input_filepath = fp,
-							noise_ratio = nr,
-							noise_period = np,
-							threeopt_threshold = tt
-						))
-
-					total += res
-
-				if total >= best_total:
-					best_total = total
-					best_nr = nr
-					best_np = np
-					best_tt = tt
-
-	print("Max score: {}, params: ({},{},{})", best_total, best_nr, best_np, best_tt)
+	if output_filepath is not None:
+		results_to_csv(results_by_length, output_filepath)
+		
 
 
 parallel_bf()
+
